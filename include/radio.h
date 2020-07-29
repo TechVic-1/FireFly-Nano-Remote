008e50b68245fb82b151950b89da51a8c02bc6ee
TechVic <victor.martinsson@hotmail.com>
2020-07-29 13:26:41 +0200

    Update radio.h

--- include/radio.h
+++ include/radio.h
@@ -10,7 +10,7 @@
 
 //#define BAND    868100000   //Hz - you can set band here directly,e.g. 868E6,915E6
 //#define PABOOST true
-#define TXPOWER 14
+#define TXPOWER 20
 #define SPREADING_FACTOR 10 // ranges from { 6 - 12 },default 7 see API docs
 #define BANDWIDTH 500000   // Hz was 500e3
 #define CODING_RATE 7      // correspond to 4/x where x is the value you give to CODING_RATE - 5 default - ranges from { 4 to 8 }
