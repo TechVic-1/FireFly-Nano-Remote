cfd230a9fb4fc66cc018326571c9f793120f4960
TechVic <victor.martinsson@hotmail.com>
2021-04-18 16:54:47 +0200

    Update receiver.cpp

--- src/receiver/receiver.cpp
+++ src/receiver/receiver.cpp
@@ -174,7 +174,7 @@ float batteryPackPercentage( float voltage ) { // Calculate the battery level of
   if (boardConfig.batteryType == 0) { // Li-ion
     minCellVoltage = 2.8;
   } else { // Li-po
-    minCellVoltage = 3.2;
+    minCellVoltage = 3.0;
   }
 
   float percentage = (100 - ( (maxCellVoltage - voltage / boardConfig.batteryCells) / ((maxCellVoltage - minCellVoltage)) ) * 100);
